44ea0284920ddf0acd5ba16f1e9d7e1d67722df8
cuyt <dphuc2304@gmail.com>
2024-08-07 05:56:34 +0700

    Fix slug error

--- src/utils/createSlug.ts
+++ src/utils/createSlug.ts
@@ -1,4 +1,4 @@
-function convertViToEn(str: string): string {
+function convertViToEn(str: string, toUpperCase = false) {
     str = str.toLowerCase();
     str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
     str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
@@ -10,9 +10,9 @@ function convertViToEn(str: string): string {
     // Some system encode vietnamese combining accent as individual utf-8 characters
     str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // Huyền sắc hỏi ngã nặng
     str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // Â, Ê, Ă, Ơ, Ư
-  
-    return str;
-  }
+
+    return toUpperCase ? str.toUpperCase() : str;
+}
   
   export default function createSlug(title: string): string {
     return convertViToEn(title)
